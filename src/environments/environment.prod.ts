92c0799015dd9de86a6a0616bf746f98e0c8a8bc
omar valdez <omar.valdez.becerril@gmail.com>
2026-01-17 18:42:28 -0600

    update endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://5wcjna0a9g.execute-api.us-west-1.amazonaws.com/prod'
+  apiUrl: 'https://12jxda4u56.execute-api.us-west-1.amazonaws.com/prod'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://5wcjna0a9g.execute-api.us-west-1.amazonaws.com/prod'
+  apiUrl: 'https://rckjx53e64.execute-api.us-west-1.amazonaws.com/prod'
 };
